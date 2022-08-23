1e290d46749541e05d3ecc6e4619e1a49789ce54
sadikulislam <sh.sifatallrafi.18@gmail.com>
2022-08-23 13:26:33 +0600

    onclik function added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,42 @@
 
- function selectPlayer(){
-   console.log('I Love You Sadia')
- }
+  const  player = [];
+  
+
+  function setPlayer(player){
+    const playerList = document.getElementById('player-list');
+    
+    for(let i = 0; i < 1; i++){
+      const name = player;
+      const li = document.createElement("li");
+      li.innerText = name;
+      playerList.appendChild(li);
+    }
+  }
+
+ function selectPlayer(event){
+   const playerName = event.parentNode.parentNode.children[1].children[0].innerText;
+  //  event.target.disabled = true;
+  //  event.target.style.background = "skyblue";
+  //  const playerList = document.getElementById('player-list');
+   
+   
+   player.push(playerName);
+
+    // event.target.disabled = true;
+
+   const totalSelectedPlayer = document.getElementById('selected').innerText = player.length;
+   
+  setPlayer(playerName);
+   
+ };
+
+ document.getElementById('calculate').addEventListener('click', function(){
+  const totalPlayer = getElementValueById('selected');
+  const perPlayerBudget = getInputValueById('per-player');
+  const playerTotalExpenses = totalPlayer * perPlayerBudget;
+
+  setElementValueById('player-expenses', playerTotalExpenses);
+ });
 
 
 
